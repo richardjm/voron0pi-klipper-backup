4489170a9062dc27f59c87fa1e5d683f608daa7d
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2022-01-23 13:04:09 +0000

    Update from 2.4 config

--- scripts/process_resonances.sh
+++ scripts/process_resonances.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-now=$(date +"%F-%X")
+now=$(date +"%F-%H_%M_%S")
 # TEST_RESONANCES AXIS=X
 # TEST_RESONANCES AXIS=Y
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_x-${now}.png
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_y-${now}.png
+
+echo Processing x-axis
+/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_x-${now}.png
+
+echo processing y-axis
+/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_y-${now}.png
+
 # TEST_RESONANCES AXIS=1,1 OUTPUT=raw_data
 # TEST_RESONANCES AXIS=1,-1 OUTPUT=raw_data
 # ~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
